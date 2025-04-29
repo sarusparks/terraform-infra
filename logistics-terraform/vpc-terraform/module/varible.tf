0dafc5401d145c7c4c4e47d687a6ddd3c9dd9bab
sarusparks <saraswathinandula53@gmail.com>
2025-04-29 11:04:10 +0530

    Update varible.tf

--- logistics-terraform/vpc-terraform/module/varible.tf
+++ logistics-terraform/vpc-terraform/module/varible.tf
@@ -233,4 +233,36 @@ variable "ami" {
 variable "instance_type" {
   default = "t3.micro"
 }
+variable "cluster_name" {
+  default = "logistics-cluster"
+}
+variable "cluster_version" {
+  default = "1.31"
+}
+variable "node_groups" {
+ 
+  description = "EKS node group configuration"
+  type = map(object({
+    instance_types = list(string)
+    node_group_name = string
+    capacity_type  = string
+    scaling_config = object({
+      desired_size = number
+      max_size     = number
+      min_size     = number
+    })
+  }))
+  default = {
+    general = {
+      instance_types = ["t3.medium"]
+      capacity_type  = "SPOT"
+      node_group_name = "logistics-node"
+      scaling_config = {
+        desired_size = 2
+        max_size     = 4
+        min_size     = 2
+      }
+    }
+  }
+}
 
