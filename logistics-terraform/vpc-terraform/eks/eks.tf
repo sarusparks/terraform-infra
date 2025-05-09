2e92b8b9a9acef580520dd60743351c906aa5a2e
sarusparks <saraswathinandula53@gmail.com>
2025-05-09 13:21:51 +0530

    Update eks.tf

--- logistics-terraform/vpc-terraform/eks/eks.tf
+++ logistics-terraform/vpc-terraform/eks/eks.tf
@@ -1,44 +1,64 @@
 resource "aws_iam_role" "cluster" {
   name = "${var.cluster_name}-cluster-role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
         Service = "eks.amazonaws.com"
       }
+      Action = "sts:AssumeRole"
     }]
   })
+
+  tags = {
+    Name = "${var.cluster_name}-cluster-role"
+  }
 }
+
 resource "aws_iam_role_policy_attachment" "cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.cluster.name
 }
+
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   version  = var.cluster_version
   role_arn = aws_iam_role.cluster.arn
+
   vpc_config {
     subnet_ids = var.subnet_ids
   }
+
   depends_on = [
     aws_iam_role_policy_attachment.cluster_policy
   ]
 }
+
 resource "aws_iam_role" "node" {
   name = "${var.cluster_name}-node-role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
         Service = "ec2.amazonaws.com"
       }
+      Action = "sts:AssumeRole"
     }]
   })
+
+  tags = {
+    Name = "${var.cluster_name}-node-role"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
 resource "aws_iam_role_policy_attachment" "node_policy" {
   for_each = toset([
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
@@ -48,19 +68,22 @@ resource "aws_iam_role_policy_attachment" "node_policy" {
   policy_arn = each.value
   role       = aws_iam_role.node.name
 }
+
 resource "aws_eks_node_group" "main" {
-  for_each = var.node_groups
+  for_each        = var.node_groups
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = each.key
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = var.subnet_ids
-  instance_types = each.value.instance_types
-  capacity_type  = each.value.capacity_type
+  instance_types  = each.value.instance_types
+  capacity_type   = each.value.capacity_type
+
   scaling_config {
     desired_size = each.value.scaling_config.desired_size
     max_size     = each.value.scaling_config.max_size
     min_size     = each.value.scaling_config.min_size
   }
+
   depends_on = [
     aws_iam_role_policy_attachment.node_policy
   ]
